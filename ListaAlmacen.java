1043349af2b797da783d8b05ffba475eeb5823d6
Giann92 <desumma.gianfranco@gmail.com>
2024-09-17 19:06:50 -0300

    modif

---
+++ ListaAlmacen.java
@@ -0,0 +1,12 @@
+
+public class ListaAlmacen {
+
+ public static void main(String[] args) {
+Almacen alm = new Almacen();
+alm.listaProductos();  
+alm.mostrarProductos();
+alm.productoMasCaro();
+alm.productoMasBarato();
+    }
+
+}
